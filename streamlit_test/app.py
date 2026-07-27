19df76ac7dc7fc09dd35692608a90819c3d13bfc
Wumingshaoxia <146912246+Wumingshaoxia@users.noreply.github.com>
2026-07-27 23:15:56 +0800

    Update app.py

--- streamlit_test/app.py
+++ streamlit_test/app.py
@@ -220,15 +220,15 @@ if st.button("生成 Word"):
         #    last_p = combined_doc.paragraphs[-1]._element
         #   last_p.getparent().remove(last_p)
         # 删除最后多余分页符
-        def remove_last_page_break(doc):
-            for p in reversed(doc.paragraphs):
-                for run in p.runs:
-                    if "w:type=\"page\"" in run._element.xml:
-                        run._element.getparent().remove(run._element)
-                        return
+def remove_last_page_break(doc):
+    for p in reversed(doc.paragraphs):
+        for run in p.runs:
+            if "w:type=\"page\"" in run._element.xml:
+                run._element.getparent().remove(run._element)
+                 return
 
 
-        remove_last_page_break(combined_doc)
+remove_last_page_break(combined_doc)
 
 output_buffer = io.BytesIO()
 combined_doc.save(output_buffer)
